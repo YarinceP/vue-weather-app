3a27cd35828d2ff3c08549c25638570f0eae5023
YarinceP <padilla.yarince@gmail.com>
2020-07-23 02:33:24 -0600

    set up the stack

---
+++ resources/js/components/WeatherApp.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="text-white mb-8">
+        <div class="places-input text-gray-800" >
+            <input type="text" class="w-full">
+        </div>
+        <div class="weather-container font-sans w-128 max-w-lg overflow-hidden bg-gray-900 shadow-lg mt-4">
+
+        <div class="current-weather flex items-center justify-between px-6 py-8">
+            <div>One</div>
+            <div>Two</div>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
