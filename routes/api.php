c2ca597eca3afd24949e2201972f43c066cb4ddb
YarinceP <padilla.yarince@gmail.com>
2020-07-27 16:37:17 -0600

    add and configure endpoints to get data

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
+use Zttp\Zttp;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/weather', function () {
+    $lat = request('lat');
+    $lon = request('lon');
+    $apiKey = config('services.open_weather.key');
+    $response = Http::get("https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&exclude=&appid=$apiKey&units=metric&lang=es");
+    return $response->json();
 });
